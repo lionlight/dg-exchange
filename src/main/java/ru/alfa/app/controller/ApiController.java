aa3fbd7842bb41c1b39cbb5d595561ed245f52db
Dmitry <88512563+lionlight@users.noreply.github.com>
2022-06-04 00:01:14 +0400

    change ApiController, ApiControllerTest

--- src/main/java/ru/alfa/app/controller/ApiController.java
+++ src/main/java/ru/alfa/app/controller/ApiController.java
@@ -3,6 +3,7 @@ package ru.alfa.app.controller;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,19 +12,25 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
 import ru.alfa.app.client.ExchangeRateClient;
 import ru.alfa.app.client.GiphyClient;
+import ru.alfa.app.model.Rate;
 import ru.alfa.app.services.ExchangeRatesService;
 import ru.alfa.app.services.GiphyService;
-import ru.alfa.app.utils.DateUtils;
 
 import java.io.IOException;
+import java.util.Locale;
+
+import static ru.alfa.app.services.enums.OpenExchangeRatesTickers.valueOf;
+import static ru.alfa.app.utils.DateUtils.getCurrentDateAsFormattedString;
+import static ru.alfa.app.utils.DateUtils.getYesterdaysDateAsFormattedString;
 
 @RestController
 @RequestMapping("/v1/")
+@Slf4j
 @AllArgsConstructor
 @Api(value = "ApiController", description = "Output the result/Выдать результат", tags = {"ApiController"})
 public class ApiController {
 
-    public static final String API_V_1_REDIRECT_URL = "/api/v1/gifs?tag=";
+    public static final String API_V_1_REDIRECT_URL = "/api/v1/gifs";
     private GiphyClient giphyClient;
     private ExchangeRateClient exchangeRateClient;
     private ExchangeRatesService exchangeRatesService;
@@ -34,19 +41,46 @@ public class ApiController {
             value = "/rates",
             produces = MediaType.APPLICATION_JSON_VALUE
     )
-    public RedirectView getRates(String base) {
+    public RedirectView getRates(String base) throws IOException {
+
         final String APP_ID = exchangeRatesService.getAppId();
         final String GENERAL_BASE = exchangeRatesService.getGeneralBase();
 
-        final DateUtils dateUtils = new DateUtils();
+        final String nowDate = getCurrentDateAsFormattedString();
+        final String yesterdayDate = getYesterdaysDateAsFormattedString();
 
-        var now = exchangeRateClient.getRates(dateUtils.getNowDateAsString() + ".json", APP_ID, GENERAL_BASE);
+        var now = exchangeRateClient.getRates(nowDate + ".json", APP_ID, GENERAL_BASE);
 
-        var yesterday = exchangeRateClient.getRates(dateUtils.getYesterdayAsString() + ".json", APP_ID, GENERAL_BASE);
+        String clientNameRequestWithNowDate = now.request().requestTemplate().feignTarget().name();
 
-        if (exchangeRatesService.isNowRateGreatest(now, yesterday, base)) {
-            return new RedirectView(API_V_1_REDIRECT_URL + giphyService.getPositiveTag());
-        } else return new RedirectView(API_V_1_REDIRECT_URL + giphyService.getNegativeTag());
+        log.info(clientNameRequestWithNowDate
+                + " send request to "
+                + exchangeRatesService.getServiceUrl()
+                + " with now date: ");
+
+        var yesterday = exchangeRateClient.getRates(yesterdayDate + ".json", APP_ID, GENERAL_BASE);
+
+        String clientNameRequestWithYesterdayDate = yesterday.request().requestTemplate().feignTarget().name();
+
+        log.info(clientNameRequestWithYesterdayDate
+                + " send request to "
+                + exchangeRatesService.getServiceUrl()
+                + " with yesterday date (for compare rates): ");
+
+        final Rate nowRate = exchangeRatesService.getRate(now, valueOf(base.toUpperCase(Locale.ROOT)));
+        final Rate yesterdayRate = exchangeRatesService.getRate(yesterday, valueOf(base.toUpperCase(Locale.ROOT)));
+
+        String redirectQuery = "?tag=";
+
+        if (exchangeRatesService.isNowRateGreatest(nowRate, yesterdayRate)) {
+            log.info("Redirect to " + API_V_1_REDIRECT_URL);
+
+            return new RedirectView(API_V_1_REDIRECT_URL + redirectQuery + giphyService.getPositiveTag());
+        } else {
+            log.info("Redirect to " + API_V_1_REDIRECT_URL);
+
+            return new RedirectView(API_V_1_REDIRECT_URL + redirectQuery + giphyService.getNegativeTag());
+        }
     }
 
     @ApiOperation(value = "gif from giphy api", notes = "")
@@ -56,6 +90,7 @@ public class ApiController {
     )
     public RedirectView getGifs(@RequestParam String tag) throws IOException {
         var response = giphyClient.getGif(giphyService.getApiKey(), tag);
+        log.info("Client send request to " + giphyService.getServiceUrl());
         return new RedirectView(giphyService.getGifUrl(response));
     }
 
--- src/test/java/ru/alfa/app/controller/ApiControllerTest.java
+++ src/test/java/ru/alfa/app/controller/ApiControllerTest.java
@@ -1,7 +1,6 @@
 package ru.alfa.app.controller;
 
 
-import org.assertj.core.api.AssertionsForClassTypes;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -10,42 +9,28 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Optional;
 
-import static org.mockito.ArgumentMatchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrlTemplate;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrlPattern;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-//@ExtendWith(SpringExtension.class)
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-//@RequiredArgsConstructor
-//@SpringBootTest(classes = {Application.class},
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
-//@ComponentScan(basePackages = {"ru.alfa.app.controller", "ru.alfa.services", "ru.alfa.client"})
 class ApiControllerTest {
-
-    @Autowired
-    private ApiController apiController;
 
     @Autowired
     private MockMvc mockMvc;
 
     @Test
-    void getRates() throws Exception {
-        AssertionsForClassTypes.assertThat(mockMvc).isNotNull();
-        AssertionsForClassTypes.assertThat(apiController).isNotNull();
-
+    void getRates_Redirect() throws Exception {
         mockMvc.perform(get("/v1/rates").queryParam("base", "RUB"))
                 .andDo(print())
                 .andExpect(status().isFound())
-                .andExpect(redirectedUrlTemplate("/api/v1/gifs?tag=broke"));
+                .andExpect(redirectedUrlPattern("/api/v1/gifs?tag=**"));
     }
 
     @Test
     void getGifs() throws Exception {
-        AssertionsForClassTypes.assertThat(mockMvc).isNotNull();
-        AssertionsForClassTypes.assertThat(apiController).isNotNull();
         mockMvc.perform(get("/v1/gifs").queryParam("tag", Optional.of("rich").orElse("broke")))
                 .andDo(print())
                 .andExpect(status().isFound());
