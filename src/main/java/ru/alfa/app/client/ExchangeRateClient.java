bd05b0b260684d0f551c86805d8bdcf3342ad615
Dmitry <88512563+lionlight@users.noreply.github.com>
2022-06-04 00:03:14 +0400

    change ExchangeRateClient

--- src/main/java/ru/alfa/app/client/ExchangeRateClient.java
+++ src/main/java/ru/alfa/app/client/ExchangeRateClient.java
@@ -1,13 +1,13 @@
 package ru.alfa.app.client;
 
 
+import feign.Response;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.alfa.app.client.config.ClientConfiguration;
-import ru.alfa.app.model.OpenEx;
 
 @FeignClient(
         value = "exchange-rates-client",
@@ -20,8 +20,7 @@ public interface ExchangeRateClient {
             value = "/api/historical/{date}",
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    OpenEx getRates(@RequestParam String date,
-                    @RequestParam("app_id") String appId,
-                    @RequestParam("base") String base);
-
+    Response getRates(@RequestParam String date,
+                      @RequestParam("app_id") String appId,
+                      @RequestParam("base") String base);
 }
